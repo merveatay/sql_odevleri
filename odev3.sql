c2448d77d48ef981945e93e932e5e578f8580dfe
Merve nur Atay <mervenuratay@macbook-air.mynet>
2021-12-11 21:03:41 +0300

    Lıke, ilike operatörlerinin alıştırmasını içeren ödev yüklendi.

---
+++ odev3.sql
@@ -0,0 +1,12 @@
+SELECT country FROM country
+WHERE country LIKE 'A%a'
+
+SELECT country FROM country
+WHERE country LIKE '___%__%n'
+
+SELECT title from film
+WHERE title ILIKE '%T%T%T%T%'
+
+SELECT * from film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
+ 
