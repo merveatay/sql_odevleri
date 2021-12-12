30c07f0df505d94fd1686bb2c344fa632e11d7eb
Merve nur Atay <mervenuratay@macbook-air.mynet>
2021-12-12 15:05:36 +0300

    Order by, limit, offset keyword ve clause'ları alıştırmalarını içeren ödev5 yüklendi.

---
+++ odev5.sql
@@ -0,0 +1,13 @@
+--order by anahtar sözcüğü koşullardan sonra yazılmalı
+--a<z ve asc artan olacak şekilde sıralama yapar
+SELECT * FROM film
+WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5
+
+SELECT * FROM film
+WHERE title LIKE '%n' ORDER BY length ASC OFFSET 5 LIMIT 5
+
+SELECT last_name, store_id FROM customer
+WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4
+
+
+
